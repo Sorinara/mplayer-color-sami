12c98eda4842ede9b2404e37be389ec3383b1477
Sorinara <playos@naver.com>
2013-09-26 21:49:32 +0900

    add normal run (debug) mode in bug_checker.sh

--- test_code/bug_checker.sh
+++ test_code/bug_checker.sh
@@ -2,6 +2,7 @@ DIRECTORY_PATH="$1"
 
 . ./subtitle_line.sh
 
+rm -rf "/tmp/valgrind_dbg_log_*"
 BINARY_FILEPATH="./subreader"
 SOURCE_FILEPATH="./subreader.c"
 COMPILE_LOG_FILEPATH="/tmp/memory_checker_compile_log_$$"
@@ -10,36 +11,73 @@ DEBUGER_LOG_FILEPATH="/tmp/valgrind_dbg_log_$$"
 SUBTITLE_VALID_LINE_FILEPATH="./result_memory_checker"
 ERROR_FILEPATH=""
 
-rm -rf "/tmp/valgrind_dbg_log_*"
+function Debug_Valgrind()
+{
+    local Binary_filepath="$1"
+    local Subtitle_valid_line_filepath="$2"
+    local Debuger_log_filepath="$3"
+
+    valgrind -v --leak-check=full --show-reachable=yes "$Binary_filepath" "$Subtitle_valid_line_filepath" >/dev/null 2>"$Debuger_log_filepath"
+    if [ -z "$(fgrep 'ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)' "$Debuger_log_filepath")" ];then
+        return 1
+    else
+        return 0
+    fi
+}
 
 if [ -z "$DIRECTORY_PATH" ];then
     echo 'insert directory path'
     exit 1
 fi
 
+echo 'Step 1> start compile'
 gcc -Wall -g -o "$BINARY_FILEPATH" "$SOURCE_FILEPATH" 2>"$COMPILE_LOG_FILEPATH"
+if [ $? != 0 ];then
+    echo 'compile failed'
+    exit 2
+fi
+
+echo -n 'Step 2> select debug mode (v : valgrind, else : normal run) :'
+read Mode
+
+if [ "$Mode" == "v" ];then
+    DEBUG_FLAG=1
+else
+    DEBUG_FLAG=0
+fi
 
 find "$DIRECTORY_PATH" -iname '*.smi' > "$SUBTITLE_FILELIST"
+if [ ! -s "$SUBTITLE_FILELIST" ];then
+    echo 'not exist sami file in target directory'
+    exit 3
+fi
+
+echo 'Step 3> check sami files'
 
 while read LINE;do
     echo -n "Check "$(basename "$LINE")" ..."
 
     Subtitle_Line_Get "$LINE" "$SUBTITLE_VALID_LINE_FILEPATH"
     if [ $? != 0 ];then
-        echo '[FAILED'
+        echo '[FAILED]'
         break
     fi
 
-    valgrind -v --leak-check=full --show-reachable=yes "$BINARY_FILEPATH" "$SUBTITLE_VALID_LINE_FILEPATH" >/dev/null 2>"$DEBUGER_LOG_FILEPATH"
-
-    if [ -z "$(fgrep 'ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)' "$DEBUGER_LOG_FILEPATH")" ];then
-        echo ERROR FILE PATH : "$LINE" >> "$DEBUGER_LOG_FILEPATH"
-        ERROR_FILEPATH="$LINE"
-        echo "[FAILED]"
-        break
+    if [ "$DEBUG_FLAG" = 1 ];then
+        Debug_Valgrind "$BINARY_FILEPATH" "$SUBTITLE_VALID_LINE_FILEPATH" "$DEBUGER_LOG_FILEPATH"
+        EXIT_STATUS=$?
     else
+        "$BINARY_FILEPATH" "$SUBTITLE_VALID_LINE_FILEPATH" 2>"$DEBUGER_LOG_FILEPATH"
+        EXIT_STATUS=$?
+    fi
+
+    if [ $EXIT_STATUS = 0 ];then
         rm -rf "$DEBUGER_LOG_FILEPATH" "$SUBTITLE_VALID_LINE_FILEPATH"
         echo "[OK]"
+    else
+        echo ERROR FILE PATH : "$LINE" >> "$DEBUGER_LOG_FILEPATH"
+        ERROR_FILEPATH="$LINE"
+        break
     fi
 done < "$SUBTITLE_FILELIST"
 
